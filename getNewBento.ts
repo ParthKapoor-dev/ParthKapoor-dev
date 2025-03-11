b2fb6bd9b7ed935abce1262ef0f0c6992ccaa695
Parth Kapoor <parthkapoor.coder@gmail.com>
2025-03-11 09:20:20 +0530

    Bento Init

---
+++ getNewBento.ts
@@ -0,0 +1,21 @@
+const apiUrl = "https://opbento.edgexhq.tech/api/bento?n=Parth%20Kapoor&g=parthkapoor-dev&x=parthkapoor_te&l=parthkapoor08&i=https%3A%2F%2Fgithub.com%2Fparthkapoor-dev.png&p=&z=5170e";
+interface BentoResponse {
+  url: string;
+}
+
+const fetchBentoUrl = async (apiUrl: string): Promise<string> => {
+  try {
+    const response = await fetch(apiUrl);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data: BentoResponse = (await response.json()) as BentoResponse;
+    return data.url;
+  } catch (error) {
+    console.error("Error fetching Bento URL:", error);
+    throw error;
+  }
+};
+
+// @ts-ignore
+fetchBentoUrl(apiUrl);
